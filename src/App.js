1212585a6f50b7c29b1ba343e161043a9d478537
Ruchila Baruah <baruah.ruchila@gmail.com>
2021-10-07 14:33:40 +0200

    Using React Transition group instead of CSS transition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import { Transition } from "react-transition-group";
 import "./App.css";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
@@ -16,7 +17,27 @@ function App() {
   return (
     <div className="App">
       <h1>React Animations</h1>
-      {modalOpen && <Modal isOpen={modalOpen} closed={onCloseHandler} />}
+      {/* Timeout is the time between 'entering' and 'entered', 'exiting' and 'exited'. It can be just a number,
+      same like the time mentioned in CSS animation. If you want to add define different timings for entering and 
+      exiting, then create an object like above 'animationTiming'. Lifecycle hooks like onEnter, onEntering etc are
+      also attached to this, which we can use for customization */}
+      {/* Method 1: */}
+      {/* <Transition
+        in={modalOpen}
+        timeout={animationTiming}
+        mountOnEnter
+        unmountOnExit
+      >
+        {(state) => {
+          return (
+            <div>
+              <Modal show={state} closed={onCloseHandler} />
+            </div>
+          );
+        }}
+      </Transition> */}
+      {/* Method 2: We can also directly use Transition inside Modal.js file and convert it into a transition component */}
+      <Modal show={modalOpen} closed={onCloseHandler} />
       {modalOpen && <Backdrop />}
       <button className="Button" onClick={onOpenModalHandler}>
         Open Modal
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,57 @@
 import React from "react";
+import { CSSTransition, Transition } from "react-transition-group";
 
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 300,
+  exit: 1000,
+};
+
 const Modal = (props) => {
-  const cssClasses = ["Modal", props.isOpen ? "ModalOpen" : "ModalClosed"];
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    // Method 1: We can use Transition if we want to manually assign the classes as done below.
+    /* <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition> */
+
+    // Method 2: Use CSSTransition if you want the package to handle the CSS classes. Check the Modal.css file
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames="fade-slide"
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
